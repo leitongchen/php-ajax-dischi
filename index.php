de1e894528498dc607390eef80e6a64ed862700e
leitong <leitongchen93@gmail.com>
2021-06-03 15:44:50 +0200

    Scaffolding

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+$discList = include "./db/disc.php";
+
+
+var_dump($discList);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="dist/app.css">
+
+    <title>Music</title>
+</head>
+<body>
+    
+</body>
+</html>
